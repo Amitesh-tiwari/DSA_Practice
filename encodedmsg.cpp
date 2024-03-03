856dcfc998c2a45b1c95f95440790625bf7cdfa8
Amitesh-tiwari <113535850+Amitesh-tiwari@users.noreply.github.com>
2024-03-03 21:04:35 +0530

    GraphsAdjacencyList

--- encodedmsg.cpp
+++ encodedmsg.cpp
@@ -1,21 +1,25 @@
-You have been given a text message. You have to return the Run-length Encoding of the given message.
+You have been given a text message.You have to return the Run - length Encoding of the given message.
 
-Run-length encoding is a fast and simple method of encoding strings. 
-The basic idea is to represent repeated successive characters as the character and a single count.
-For example, the string "aaaabbbccdaa" would be encoded as "a4b3c2d1a2".
+ Run -
+    length encoding is a fast and
+    simple method of encoding strings.The basic idea is to represent repeated successive characters as the character and a single count.For example,
+    the string "aaaabbbccdaa" would be encoded as "a4b3c2d1a2".
 
-//code
+// code
 
-#include <bits/stdc++.h> 
-string encode(string &message)
+#include <bits/stdc++.h>
+    string
+    encode(string &message)
 {
-    int n = message.size(),i;
+    int n = message.size(), i;
     string encodedmsg = "";
-    for(i=0;i<n;i++){
+    for (i = 0; i < n; i++)
+    {
 
         char curChar = message[i];
         int charFreq = 1;
-        while(i+1 <n &&message[i+1]== curChar){
+        while (i + 1 < n && message[i + 1] == curChar)
+        {
             i++;
             charFreq++;
         }
