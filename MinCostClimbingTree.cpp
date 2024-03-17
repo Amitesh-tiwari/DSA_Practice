c4db7ce7f1384decb4fc737d0c5be8cbef2a8b0e
Amitesh-tiwari <113535850+Amitesh-tiwari@users.noreply.github.com>
2024-03-17 23:34:59 +0530

    BottomUpApproach

--- MinCostClimbingTree.cpp
+++ MinCostClimbingTree.cpp
@@ -44,3 +44,25 @@ public:
         
     }
 };
+
+//bottom up approach
+ int solve(vector<int>& cost, int n)
+    {
+        vector<int> dp(n+1);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        for(int i=2;i<n;i++){
+            dp[i] = cost[i] + min(dp[i-1], dp[i-2]);
+        }
+
+       return min(dp[n-1], dp[n-2]);
+
+    }
+
+    int minCostClimbingStairs(vector<int>& cost) {
+
+        int n = cost.size();
+        return solve(cost,n);
+    }
+};
