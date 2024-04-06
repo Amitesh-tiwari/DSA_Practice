8d9e00caed32a606d9eaf6eae979f3793b265d30
Amitesh-tiwari <113535850+Amitesh-tiwari@users.noreply.github.com>
2024-04-06 23:42:46 +0530

    Optimized Solution

--- LongestIncreasingSubsequence.cpp
+++ LongestIncreasingSubsequence.cpp
@@ -32,3 +32,29 @@ int longestIncreasingSubsequence(int arr[], int n){
     return ans;
 }
 
+//optimiszed approach Binary Search + DP
+
+#include<algorithm>
+
+int longestIncreasingSubsequence(int arr[], int n)
+{
+    int dp[n];
+    
+    int ans = 0;
+
+    for(int i=0;i<n;i++){
+
+        int position = lower_bound(dp,dp + ans, arr[i]) - dp;
+        dp[position] = arr[i];
+
+        if(position == ans){
+            ans++;
+        }
+      
+    }
+
+    return ans;
+
+}
+
+
