29f9e523ddb15f62d46640e1e7ba2d1cd7a9c96f
Amitesh-tiwari <113535850+Amitesh-tiwari@users.noreply.github.com>
2024-09-15 23:44:03 +0530

    changes

--- AllocateBooks.cpp
+++ AllocateBooks.cpp
@@ -1,9 +1,20 @@
 Problem statement
-You have been given a permutation of ‘N’ integers. A sequence of ‘N’ integers is called a permutation if it contains all integers from 1 to ‘N’ exactly once. Your task is to rearrange the numbers and generate the lexicographically next greater permutation.
+You have been given a permutation of ‘N’ integers. 
+A sequence of ‘N’ integers is called a permutation if 
+it contains all integers from 1 to ‘N’ exactly once. Your
+ task is to rearrange the numbers and generate the 
+ lexicographically next greater permutation.
 
-To determine which of the two permutations is lexicographically smaller, we compare their first elements of both permutations. If they are equal — compare the second, and so on. If we have two permutations X and Y, then X is lexicographically smaller if X[i] < Y[i], where ‘i’ is the first index in which the permutations X and Y differ.
+To determine which of the two permutations is 
+lexicographically smaller, we compare their first elements
+ of both permutations. If they are equal — compare 
+ the second, and so on. If we have two permutations X and 
+ Y, then X is lexicographically smaller if X[i] < Y[i],
+  where ‘i’ is the first index in which the permutations 
+  X and Y differ.
 
-For example, [2, 1, 3, 4] is lexicographically smaller than [2, 1, 4, 3].
+For example, [2, 1, 3, 4] is lexicographically smaller 
+than [2, 1, 4, 3].
 
 //code
 #include <bits/stdc++.h> 
